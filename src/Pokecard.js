c3887e136c78b3cde11d97053d8d56a6d0735324
Drot88 <Daniel.Rotenberg88@gmail.com>
2019-05-06 12:57:40 -0500

    Added some styling and new image for pokemon

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,17 +1,24 @@
 import React, {Component} from 'react';
 import './Pokecard.css';
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
 
 class Pokecard extends Component {
     render() {
-        let imgsrc = `${POKE_API}${this.props.id}.png`;
+        let imgsrc = `${POKE_API}`;
+        if(this.props.id < 10) {
+            imgsrc += `00${this.props.id}.png`;
+        } else if (this.props.id < 100) {
+            imgsrc += `0${this.props.id}.png`;
+        } else {
+            imgsrc += `${this.props.id}.png`;
+        }
         return (
             <div className="Pokecard">
-               <h1>Name: {this.props.name}</h1>
-               <h2>ID:{this.props.id}</h2>
-               <h2>Type:{this.props.type}</h2>
-               <h2>Exp:{this.props.exp}</h2>
+               <h1 className="Pokecard-title">Name: {this.props.name}</h1>
                <img src={imgsrc} alt={this.props.name}/>
+               {/* <h2 className="Pokecard-data">ID:{this.props.id}</h2> */}
+               <h2 className="Pokecard-data">Type: {this.props.type}</h2>
+               <h2 className="Pokecard-data">Exp: {this.props.exp}</h2>
             </div>
         )
     }
